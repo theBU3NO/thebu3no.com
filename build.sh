1e2247b83e2eb2c1c678e7400aeddbda83187b4c
theBU3NO <1988704+theBU3NO@users.noreply.github.com>
2026-01-28 09:03:08 -0500

    why isn't it recognizing the Preview branch

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+echo Current branch: $CF_PAGES_BRANCH
+
 if [ "$CF_PAGES_BRANCH" == "Preview" ]; then
   # Preview build command
-  echo Building Hugo site on $CF_PAGES_BRANCH branch.
+  echo Building Hugo site with Preview configuration branch.
   hugo --baseURL https://dev.thebu3no-com.pages.dev
   
 else
   # Production build command, uses baseurl from hugo.toml
-  echo Building Hugo site on Production branch.
+  echo Building Hugo site with Production configuration.
   hugo
 fi
