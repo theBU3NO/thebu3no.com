4970c1d6cd6714c0ca62ee90b115646af8ec3ff1
theBU3NO <1988704+theBU3NO@users.noreply.github.com>
2026-01-27 23:28:32 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ "$CF_PAGES_BRANCH" = "Preview" ]; then
-  # Production build command
+if [ "$CF_PAGES_BRANCH" == "Preview" ]; then
+  # Preview build command
   hugo --baseURL https://dev.thebu3no-com.pages.dev
 else
-  # Preview/staging build command (e.g., including drafts)
+  # Production build command, uses baseurl from hugo.toml
   hugo
 fi
