8109c36b27d54503fc937cfd43b077c3150bc32b
theBU3NO <1988704+theBU3NO@users.noreply.github.com>
2026-01-28 09:19:54 -0500

    so cool
    I'm not a trained developer, and I don't want to be, but I have always had an incredible love and fascination for web technology. I spent days tinkering with my geocities page, and i cried for hours when i accidentally deleted it before i had any concept of the word backup.
    
    It's just so cool when things I get things like this to work. I'm using a script to adjust the default way cloudflare pages (for free) builds a site on my custom domain from my GitHub repo.

--- build.sh
+++ build.sh
@@ -2,13 +2,15 @@
 
 echo Current branch: $CF_PAGES_BRANCH
 
+  # This is needed for hugo to create the correct links stylesheets and other files when switching between Production ("live" GitHub branch) and Preview ("dev" GitHub branch) domain URLs.
+
 if [ "$CF_PAGES_BRANCH" == "dev" ]; then
-  # Preview build command
-  echo Building Hugo site with Preview ("dev") configuration branch.
+  # Preview build command, uses Cloudflare pages alias url for baseURL.
+  echo "Building Hugo site with Preview ("dev") configuration branch."
   hugo --baseURL https://dev.thebu3no-com.pages.dev
   
 else
-  # Production build command, uses baseurl from hugo.toml
-  echo Building Hugo site with Production ("live") configuration.
+  # Production build command, uses baseURL from hugo.toml
+  echo "Building Hugo site with Production ("live") configuration."
   hugo
 fi
