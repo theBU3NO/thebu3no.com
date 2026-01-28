d872962b1ae43130799fc3620f3bd889848aae1e
theBU3NO <1988704+theBU3NO@users.noreply.github.com>
2026-01-28 09:06:24 -0500

    because it uses the github branch for the names

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 
 echo Current branch: $CF_PAGES_BRANCH
 
-if [ "$CF_PAGES_BRANCH" == "Preview" ]; then
+if [ "$CF_PAGES_BRANCH" == "dev" ]; then
   # Preview build command
-  echo Building Hugo site with Preview configuration branch.
+  echo Building Hugo site with Preview ("dev") configuration branch.
   hugo --baseURL https://dev.thebu3no-com.pages.dev
   
 else
   # Production build command, uses baseurl from hugo.toml
-  echo Building Hugo site with Production configuration.
+  echo Building Hugo site with Production ("live") configuration.
   hugo
 fi
